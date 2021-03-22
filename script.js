5d1b400a7134f59a0f65de37d83c629444f3c1ba
Rachel Strong <rachellynnstrong@gmail.com>
2021-03-22 18:19:18 -0400

    adding the basic html files and style sheets. Used bulma

---
+++ script.js
@@ -0,0 +1,6 @@
+
+
+$('.btn').on('click', function(e){
+    e.preventDefault();
+    window.location.href='file:///C:/Users/rache/Desktop/Developer%20Bootcamp/whatsthat/assets/results-page.html';
+  });
