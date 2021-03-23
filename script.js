cac7451138764461d14c88ccef15051ceadca27b
Rachel Strong <rachellynnstrong@gmail.com>
2021-03-23 00:24:40 -0400

    minor tweaks

--- script.js
+++ script.js
@@ -3,4 +3,6 @@
 $('.btn').on('click', function(e){
     e.preventDefault();
     window.location.href='file:///C:/Users/rache/Desktop/Developer%20Bootcamp/whatsthat/assets/results-page.html';
+
   });
+
